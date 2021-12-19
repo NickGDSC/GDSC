5f4808220f4cc38f4e93c0adae7d05d92a1ad4f5
Nick <melgotchenko_n@iuca.kg>
2021-12-19 15:41:29 +0600

    My first commit

---
+++ variables.py
@@ -0,0 +1,34 @@
+print("#1 bool")
+a = True
+print(type(a))
+
+print("#2")
+print("1.1 int")
+a = 11
+print(type(a))
+print("1.2 float")
+a = 1.1
+print(type(a))
+print("1.3 complex")
+a = 1+2j
+print(type(a))
+
+print("#3 str")
+a = "Nikita"
+print(a)
+print(type(a))
+
+print("#4 list")
+a = list("Nikita")
+print(a)
+print(type(a))
+
+print("#5 set")
+a = set("Nikita")
+print(a)
+print(type(a))
+
+print("#6 dict")
+a = dict(first = "a" , second = "b")
+print(a)
+print(type(a))
